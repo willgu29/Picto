bb351077519c4d4c826b3f0e617996ada193271b
Will Gu <willgu29@gmail.com>
2014-08-27 18:11:44 -0700

    19 seconds

--- Twerk/MapViewController.m
+++ Twerk/MapViewController.m
@@ -846,7 +846,7 @@ typedef NSInteger AnnotationCheck;
     
     // Create a dispatch source that'll act as a timer on the concurrent queue
     dispatchSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
-    double interval = 30.0;
+    double interval = 19.0;
     dispatch_time_t startTime = dispatch_time(DISPATCH_TIME_NOW, 0);
     uint64_t intervalTime = (int64_t)(interval * NSEC_PER_SEC);
     dispatch_source_set_timer(dispatchSource, startTime, intervalTime, 0);
