1b809ad1d3546f049aa0c7c8c24bb759ae6141df
Will Gu <willgu29@gmail.com>
2014-09-04 21:34:47 -0700

    Added and changed xib constraints

---
+++ Twerk/PictureArray.h
@@ -0,0 +1,13 @@
+//
+//  PictureArray.h
+//  Picto
+//
+//  Created by William Gu on 9/4/14.
+//  Copyright (c) 2014 William Gu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PictureArray : NSObject
+
+@end
---
+++ Twerk/PictureArray.m
@@ -0,0 +1,13 @@
+//
+//  PictureArray.m
+//  Picto
+//
+//  Created by William Gu on 9/4/14.
+//  Copyright (c) 2014 William Gu. All rights reserved.
+//
+
+#import "PictureArray.h"
+
+@implementation PictureArray
+
+@end
