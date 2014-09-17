0917813ac5a6bf20475c9cdd5c62356348cfebeb
Will Gu <willgu29@gmail.com>
2014-09-17 14:33:03 -0700

    Added files for background stuff

---
+++ Twerk/BackgroundOperations.h
@@ -0,0 +1,14 @@
+//
+//  BackgroundOperations.h
+//  Picto
+//
+//  Created by William Gu on 9/17/14.
+//  Copyright (c) 2014 William Gu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface BackgroundOperations : NSObject <CLLocationManagerDelegate>
+
+@end
---
+++ Twerk/BackgroundOperations.m
@@ -0,0 +1,20 @@
+//
+//  BackgroundOperations.m
+//  Picto
+//
+//  Created by William Gu on 9/17/14.
+//  Copyright (c) 2014 William Gu. All rights reserved.
+//
+
+#import "BackgroundOperations.h"
+
+@implementation BackgroundOperations
+
+//- (void)locationManager:(CLLocationManager *)manager
+//               didVisit:(CLVisit *)visit
+//{
+//    
+//}
+
+
+@end
